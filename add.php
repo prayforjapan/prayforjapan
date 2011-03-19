8a69409096bcc623105bbb27ddba2ec500f110ab
Charley Ramm <charleyramm+pfj@gmail.com>
2011-03-19 16:48:32 -0400

    Comment  in add.php view

--- add.php
+++ add.php
@@ -3,7 +3,7 @@
 	include './add_c.inc'; 	// Controller
 	head();
 ?>
-	<p><strong><?php echo $message; ?></strong></p>
+	<p><strong><?php echo $message; // Prints messages about whether the submission of the form was successful ?></strong></p>
 	<form action="add.php" method="post">
 		<textarea name="description" cols="40" rows="10"></textarea>
 		<?php 
